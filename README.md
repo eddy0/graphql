98f648f130f53781cc911f49a6b0cc27874fc694
eddy0 <awindy369@gmail.com>
2018-06-23 19:06:08 -0700

    graphql server side

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # graphql
 graphql + react
+
+
+
+
+#### naming the item and useage fragment
+```
+// GraphQL
+{
+  google: company(id: "1") {
+    ...list
+  }
+  apple: company(id: "2") {
+    name
+    id
+  }
+}
+
+fragment list on company {
+  name,
+  id
+}
+```
+
+#### GraphQNonNull
+similar to required, if not provided, then throw error
---
+++ server.js
@@ -0,0 +1,33 @@
+const log = console.log.bind(console)
+
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+
+const app = express()
+
+const congfigApp = () => {
+    const schema = require('./schema/schema')
+    app.use('/graphql', new expressGraphQL({
+        graphiql: true,
+        schema: schema,
+    }))
+
+}
+
+const run = (port, host) => {
+    const server = app.listen(port, host, () => {
+        const address = server.address()
+        log(`listening ${address.address}:${address.port}`)
+    })
+}
+
+const __main = () => {
+    congfigApp()
+    const port = 4000
+    const host = 'localhost'
+    run(port, host)
+}
+
+if (require.main === module) {
+    __main()
+}
